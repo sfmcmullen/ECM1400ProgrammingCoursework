4b223ded79d9d471ddf6ada5fb4b475945184449
sfmcmullen <sfmcmullen@gmail.com>
2023-11-09 17:02:08 +0000

    PEP8 Formatting

--- game_engine.py
+++ game_engine.py
@@ -5,15 +5,15 @@ def attack(coordinates, board, battleships):
     """Return true if the coordinate is a ship, else return false.
     If the ship value is 0 in battleships, then the ship is sunk."""
 
-    if board[coordinates[0]][coordinates[1]] != None:
+    if board[coordinates[0]][coordinates[1]] is not None:
         #Decrement the value of remaining pieces in the battleships dictionary
         battleships[board[coordinates[0]][coordinates[1]]] -= 1
         #Set the square to 0 as that area has been sunk
         board[coordinates[0]][coordinates[1]] = None
         return True
-    else: 
-        return False
-    
+
+    return False
+
 
 def cli_coordinates_input():
     """Takes inputs for x, y cooridnates and returns tuple."""
@@ -24,21 +24,20 @@ def cli_coordinates_input():
 
 
 def single_game_loop():
-    """"""
+    """Runs a single loop of the game."""
 
     print("~~~~~ Welcome to Battleships! ~~~~~")
     board = c.initialise_board() #create board
     battleships = c.create_battleships() #create ships
     board = c.place_battleships(board, battleships) #add ships to board
-    battleships.values
-    
+
     non_zero_exists = True
     while non_zero_exists:
 
         coordinates = cli_coordinates_input()
         result = {True: "Hit", False: "Miss"}
         print(result[attack(coordinates, board, battleships)])
-        print("Current Board: ") #FOR TESTING. REMOVE LATER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        print("Current Board: ") #FOR TESTING. REMOVE LATER!!!!!!!!!!!!!!
         print(board)
         print("")
 
@@ -47,7 +46,6 @@ def single_game_loop():
             if value != 0:
                 non_zero_exists = True
                 break
-            else:
-                non_zero_exists = False
-    
+            non_zero_exists = False
+
     print("~~~~~ Game Over ! ~~~~~")
